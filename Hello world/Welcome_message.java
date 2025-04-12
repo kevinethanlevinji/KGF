0ab73b0dffda0da34d47583aff419b55482bf185
kevinethanlevinji <kevinethanlevinji@gmail.com>
2025-04-12 19:28:44 +0000

    this is first commit from branch

--- Hello world/Welcome_message.java
+++ Hello world/Welcome_message.java
@@ -1,5 +1,6 @@
 public class Welcome_message {
     public static void main(String[] args) {
         System.out.println("Hello world welcome to KGF");
+        System.out.println("trying to clone from terminal");
     }
 }
