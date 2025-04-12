3de7140ad07fe86ccc9fc09da8f8a45afe806d2f
TharunDurganala <durganalatharun@gmail.com>
2025-04-12 18:43:27 +0000

    added semicolon

--- Hello world/Welcome_message.java
+++ Hello world/Welcome_message.java
@@ -1,6 +1,6 @@
 public class Welcome_message {
     public static void main(String[] args) {
         System.out.println("Hello world welcome to KGF");
-        System.out.println("Hello hope you are doing well")
+        System.out.println("Hello hope you are doing well");
     }
 }
