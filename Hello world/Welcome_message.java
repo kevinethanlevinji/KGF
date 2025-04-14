e891ea5ecbcce7bf1df61860214cf7be1548e78b
TharunDurganala <durganalatharun@gmail.com>
2025-04-14 11:54:51 +0000

    adding code from branch

--- Hello world/Welcome_message.java
+++ Hello world/Welcome_message.java
@@ -2,5 +2,6 @@ public class Welcome_message {
     public static void main(String[] args) {
         System.out.println("Hello world welcome to KGF");
         System.out.println("Hello hope you are doing well");
+        System.out.println("how to be happy brother");
     }
 }
