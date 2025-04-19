1daee369da498a43731c0e084f83d1c50656c52c
kevinethanlevinji <kevinethanlevinji@gmail.com>
2025-04-19 12:36:50 +0530

    For Praticing Purpose

--- Hello world/Welcome_message.java
+++ Hello world/Welcome_message.java
@@ -5,6 +5,6 @@ public class Welcome_message {
         System.out.println("how to be haappy");
         System.out.println("if you love something set it free");
         System.out.println("iam kaveri, practising  github");
-        Sysoout.out.println("Am From Warangal");
+        System.out.println("Am From Warangal");
     }
 }
